3cd19203b1c76cb021243ee972ebd3f54f337f43
Leeseoleem <leeseorim0029@gmail.com>
2024-09-25 21:14:47 +0900

    font-gray-color-import
    폰트 저장
    폰트 스타일 지정
    그레이 스케일 스타일 지정

---
+++ GlassBottle/assets/colors/theme_colors.js
@@ -0,0 +1,12 @@
+export const Gray_Color = {
+  black: "#1E1E1E",
+  gray_90: "#333333",
+  gray_80: "#454545",
+  gray_70: "#555555",
+  gray_60: "#676767",
+  gray_50: "#767676",
+  gray_40: "#888888",
+  gray_30: "#999999",
+  gray_20: "#D0D0D0",
+  white: "#fff",
+};
---
+++ GlassBottle/assets/fonts/fontSyle.js
@@ -0,0 +1,30 @@
+export const font_styles = {
+  header: {
+    fontFamily: "Pretendard-Bold",
+    fontSize: 28,
+  },
+  title_01: {
+    fontFamily: "Pretendard-Bold",
+    fontSize: 24,
+  },
+  title_02: {
+    fontFamily: "Pretendard-SemiBold",
+    fontSize: 20,
+  },
+  subtitle: {
+    fontFamily: "Pretendard-SemiBold",
+    fontSize: 16,
+  },
+  body: {
+    fontFamily: "Pretendard-Medium",
+    fontSize: 16,
+  },
+  caption: {
+    fontFamily: "Pretendard-Medium",
+    fontSize: 14,
+  },
+  description: {
+    fontFamily: "Pretendard-Medium",
+    fontSize: 12,
+  },
+};
